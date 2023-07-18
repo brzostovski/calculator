abe1d09fa330222663165dcffad0422e31822215
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-07-18 18:51:09 +0200

    Fix display input with leading zero, make AC work

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const NUMBER_KEYS = document.querySelectorAll('.number');
 const OPERATOR_KEYS = document.querySelectorAll('.operator');
 const FUNC_KEYS = document.querySelectorAll('.func-button');
 const DISPLAY = document.querySelector('#display');
+const AC = document.querySelector('#AC');
 
 function buttonDeactivate(button) {
   button.classList.remove('active');
@@ -22,8 +23,17 @@ NUMBER_KEYS.forEach(button => {
       DISPLAY.textContent = button.textContent;
     } else if (DISPLAY.textContent.length > 9) {
       return;
+    } else if (DISPLAY.textContent === '0') {
+      if (button.textContent === '0') return;
+      DISPLAY.textContent = button.textContent;
     } else {
       DISPLAY.textContent += button.textContent;
     }
   })
+})
+
+AC.addEventListener('click', () => {
+  DISPLAY.textContent = '0';
+  DISPLAY.classList.add('result');
+  OPERATOR_KEYS.forEach(button => buttonDeactivate(button));
 })
