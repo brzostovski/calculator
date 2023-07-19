38c623c100e87188df18410409d208f9ad776cca
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-07-19 17:22:36 +0200

    Fix operator buttons behavior
    This commit fixes equals sign behavior i.e. clicking equals button
    repeatedly will just repeat last operation with last input number and
    previous result.
    It also fixes equals sign animation but changing index.html such that
    class .active is never added to equals button and animation works
    purely by css :active property.
    Clicking one operator button directly after clicking different operator
    button works correctly and just changes assigned operator without
    computing unwanted result.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const OPERATOR_KEYS = document.querySelectorAll('.operator');
 const FUNC_KEYS = document.querySelectorAll('.func-button');
 const DISPLAY = document.querySelector('#display');
 const AC = document.querySelector('#AC');
+const EQUALS_KEY = document.querySelector('#equals');
 
 let memory;
 let operator;
@@ -38,30 +39,54 @@ function clearEverything() {
   deactivateOperators();
 }
 
+function otherOperatorActive(clickedOperator) {
+  OPERATOR_KEYS.forEach(buttonChecked => {
+    if (buttonChecked.classList.contains('active')
+    && (buttonChecked.id !== clickedOperator.id)) {
+      buttonChecked.classList.remove('active');
+      clickedOperator.classList.add('active');
+      return true;
+    } else {
+      return false;
+    }
+  })
+}
+
 OPERATOR_KEYS.forEach(button => {
   button.addEventListener('click', () => {
-    deactivateOperators();
+    if (otherOperatorActive(button) === true) return;
 
-    if (button.id !== 'equals') button.classList.add('active');
+    deactivateOperators();
 
     operatorClicked = true;
     let currentDisplay = parseInt(DISPLAY.textContent);
+    let currentOperator = button.id;
 
     if ((button.id === 'equals') && (memory === undefined)) {
       return;
     } else if ((memory === undefined) || (operator === undefined)) {
+      if (currentOperator === 'equals') return;
+      button.classList.add('active');
       memory = currentDisplay;
       operator = button.id;
-      console.log(`memory: ${memory}, operator: ${operator}`)
     } else {
-      let currentOperator = button.id;
       let operationResult = operation(operator, memory, currentDisplay);
+
       if (currentOperator === 'equals') {
-        clearEverything;
+        if (!DISPLAY.classList.contains('result')) memory = currentDisplay;
+        DISPLAY.classList.add('result');
+      } else if (DISPLAY.classList.contains('result')) {
+        button.classList.add('active');
+        DISPLAY.classList.remove('result');
+        operator = currentOperator;
+        memory = currentDisplay;
+        return;
       } else {
+        button.classList.add('active');
         operator = currentOperator;
         memory = operationResult;
       }
+
       DISPLAY.textContent = operationResult;
     }
   })
@@ -70,12 +95,12 @@ OPERATOR_KEYS.forEach(button => {
 NUMBER_KEYS.forEach(button => {
   button.addEventListener('click', () => {
     if (DISPLAY.classList.contains('result')) {
+      clearEverything();
       DISPLAY.classList.remove('result');
       DISPLAY.textContent = button.textContent;
     } else if (DISPLAY.textContent.length > 9) {
       return;
     } else if ((DISPLAY.textContent === '0') || (operatorClicked === true)) {
-      //if (button.textContent === '0') return;
       DISPLAY.textContent = button.textContent;
       operatorClicked = false;
       deactivateOperators();
